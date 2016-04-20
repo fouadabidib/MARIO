d644c1c988d6645a0599f1469f183bdb105cb3c2
Julien BULOT <jbulot@outlook.fr>
2016-04-20 21:16:16 +0200

    adding descriptions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).keydown(function(e){
-	var init = $('.mario').css('left');
-    if (e.keyCode == 39 && init != '1200px') {
+	var init = $('.mario').css('left'); // Détection de la position de Mario
+    if (e.keyCode == 39 && init != '1200px') {  // Conditions pour aller vers la droite
 		if ($('.mario').hasClass("mario-g")) {
 			$('.mario').removeClass("mario-g");
 		}
 		$('.mario').css('left', '+=20');
 	}
-	else if (e.keyCode == 38) {
+	else if (e.keyCode == 38) { // Condition pour sauter
 		$('.mario').animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
 	}
-	else if (e.keyCode == 37 && init > '0px') {
+	else if (e.keyCode == 37 && init > '0px') { // Conditions pour aller vers la guche
         $('.mario').addClass('mario-g');
         $('.mario').css('left',"-=20px");
     }
