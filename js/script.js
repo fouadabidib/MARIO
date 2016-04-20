a77030e3543db86c1969af2f84cf0f875cd3f299
jbulot-aston <jbulot@outlook.fr>
2016-04-20 15:12:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-$(function() {
-    $('html').keydown(function(e) {
-        if (e.keyCode == 37) {
-            $('.mario').addClass('mario-g');
-            $('.mario').animate({left: "-=20px"});
-        }
-    });
-});
-=======
- 
-	$(document).keydown(function(e){
-
-		if(e.keyCode == 39) {
-			if ($('.mario').hasClass("mario-g")) {
-				$('.mario').removeClass("mario-g");
+$(document).keydown(function(e){
+	if(e.keyCode == 39) {
+		if ($('.mario').hasClass("mario-g")) {
+			$('.mario').removeClass("mario-g");
 			}
-		$('.mario').animate({left: '+=20'});
+			$('.mario').animate({left: '+=20'});
 		}
-
 		else if(e.keyCode == 38) {
 			$('.mario')
 			.animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
 		}
-	
-	});
->>>>>>> droite
+		else if (e.keyCode == 37) {
+            		$('.mario').addClass('mario-g');
+            		$('.mario').animate({left: "-=20px"});
+        	}
+});
