39d3bc861c4ab518e73a691f242a0f39841ae40f
Julien BULOT <jbulot@outlook.fr>
2016-04-20 16:30:44 +0200

    move good

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 $(document).keydown(function(e){
-    var init = $('.mario').css('left');
-	if (e.keyCode == 39) {
+	var init = $('.mario').css('left');
+    if (e.keyCode == 39 && init != '1200px') {
 		if ($('.mario').hasClass("mario-g")) {
 			$('.mario').removeClass("mario-g");
 		}
-		$('.mario').animate({left: '+=20'});
+		$('.mario').css('left', '+=20');
 	}
 	else if (e.keyCode == 38) {
-		$('.mario')
-		.animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
+		$('.mario').animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
 	}
-	else if (e.keyCode == 37 && init != '20px') {
+	else if (e.keyCode == 37 && init > '0px') {
         $('.mario').addClass('mario-g');
-        $('.mario').animate({left: "-=20px"});
+        $('.mario').css('left',"-=20px");
     }
 });
