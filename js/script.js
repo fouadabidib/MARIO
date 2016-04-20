c9baaa4fa863e6aeb330b43646efa690d4d6f746
Julien BULOT <jbulot@outlook.fr>
2016-04-20 15:48:46 +0200

    add block left script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 $(document).keydown(function(e){
     var init = $('.mario').css('left');
-	if(e.keyCode == 39) {
+	if (e.keyCode == 39) {
 		if ($('.mario').hasClass("mario-g")) {
 			$('.mario').removeClass("mario-g");
-			}
-			$('.mario').animate({left: '+=20'});
 		}
-		else if(e.keyCode == 38) {
-			$('.mario')
-			.animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
-		}
-		else if (e.keyCode == 37) {
-            console.log(init);
-            		$('.mario').addClass('mario-g');
-            		$('.mario').animate({left: "-=20px"});
-        	}
+		$('.mario').animate({left: '+=20'});
+	}
+	else if (e.keyCode == 38) {
+		$('.mario')
+		.animate({bottom: '+=150'}, 'slow').animate({bottom:'-=150'},'slow');
+	}
+	else if (e.keyCode == 37 && init != '20px') {
+        $('.mario').addClass('mario-g');
+        $('.mario').animate({left: "-=20px"});
+    }
 });
